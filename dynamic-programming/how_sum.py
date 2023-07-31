89d2953f3a535e7dbd732ed968037ac000d01595
michael <micha3l_255@yahoo.com>
2023-07-31 03:03:46 -0500

    how sum

--- dynamic-programming/how_sum.py
+++ dynamic-programming/how_sum.py
@@ -1,33 +1,30 @@
-def how_sum(target_sum: int, numbers: list, memoize: dict = None, test: list = None) -> list:
+from typing import Union
+
+
+def how_sum(target_sum: int, numbers: list, memoize: dict = None) -> Union[list, None]:
     if memoize is None:
         memoize = {}
-
-    if test is None:
-        test = [[]]
 
     if target_sum in memoize:
         return memoize[target_sum]
 
     if target_sum == 0:
-        return test
+        return []
 
     if target_sum < 0:
         return None
 
-    all_lists = []
     for number in numbers:
         remainder = target_sum - number
         remainder_result = how_sum(remainder, numbers, memoize)
-
         if remainder_result is not None:
-            for lst in remainder_result:
-                new_list = lst + [number]
-                all_lists.append(new_list)
+            remainder_result.append(number)
+            memoize[target_sum] = remainder_result
+            return remainder_result
 
-    memoize[target_sum] = all_lists
-
-    return all_lists
+    memoize[target_sum] = None
+    return None
 
 
 if __name__ == '__main__':
-    print(how_sum(10, [5, 2, 10]))
+    print(how_sum(1000, [99, 77]))
