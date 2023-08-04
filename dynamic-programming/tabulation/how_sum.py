cb6e0a0708aa66cebd3102d78bc82d7b67c1c945
michael <micha3l_255@yahoo.com>
2023-08-04 01:17:29 -0500

    how sum

--- dynamic-programming/tabulation/how_sum.py
+++ dynamic-programming/tabulation/how_sum.py
@@ -10,11 +10,9 @@ def how_sum(target_sum: int, numbers: list) -> list:
             if i + number < len(dp) and dp[i] is not None:
                 dp[i + number] = dp[i][:] + [number]
 
-    print(dp)
-
     return dp[target_sum]
 
 
-# print(how_sum(1000, [99, 77]))
+print(how_sum(1000, [99, 77]))
 print(how_sum(10, [3, 5, 2]))
 print(how_sum(15, [3, 5, 2]))
