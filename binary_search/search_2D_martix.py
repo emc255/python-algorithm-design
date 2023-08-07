7a02eca81ad3653d07baf940211923cf436ec942
michael <micha3l_255@yahoo.com>
2023-08-07 15:29:31 -0500

    search matrix

--- binary_search/search_2D_martix.py
+++ binary_search/search_2D_martix.py
@@ -74,5 +74,5 @@ def search_matrix_v2(matrix: list[list[int]], target: int) -> bool:
     return False
 
 
-print(search_matrix([[1, 3, 5, 7], [10, 11, 16, 20], [23, 30, 34, 60]], 23))
-print(search_matrix_v2([[1, 3, 5, 7], [10, 11, 16, 20], [23, 30, 34, 60]], 3333))
+print(search_matrix([[1, 3, 5, 7], [10, 11, 16, 20], [23, 30, 34, 60]], 60))
+print(search_matrix_v2([[1, 3, 5, 7], [10, 11, 16, 20], [23, 30, 34, 60]], 60))
