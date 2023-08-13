96edc6ceb9daa981df82627776de27ac5b2e1a05
michael <micha3l_255@yahoo.com>
2023-08-13 17:40:34 -0500

    bitwise

--- bitwise/learning.py
+++ bitwise/learning.py
@@ -1,24 +1,70 @@
-dividend = 100
-divisor = 6
-multiple = 1
+"""
+Bitwise AND (&)
+10  = 1010
+2   = 0010
+ans = 0010
+"""
+print("Bitwise AND (&)")
+a = 10
+b = 2
+ans = a & b
+print(f'a = {bin(a)[2:]}')
+print(f'b = {bin(b)[2:]}')
+print(f'ans = {bin(ans)[2:]}')
+print("===============")
 
-result = 0
+"""
+Bitwise OR (|)
+11  = 1011
+2   = 0010
+ans = 1011
+"""
+print("Bitwise OR (|)")
+a = 11
+b = 2
+ans = a | b
+print(f'a = {bin(a)[2:]}')
+print(f'b = {bin(b)[2:]}')
+print(f'ans = {bin(ans)[2:]}')
+print("===============")
 
-while dividend >= divisor:
-    temp_divisor = divisor
-    multiple = 1
+"""
+Bitwise XOR (^)
+11  = 1110
+2   = 1010
+ans = 0100
+"""
+print("Bitwise XOR (^)")
+a = 14
+b = 10
+ans = a ^ b
+print(f'a = {bin(a)[2:]}')
+print(f'b = {bin(b)[2:]}')
+print(f'ans = {bin(ans)[2:]}')
+print("===============")
 
-    while dividend >= (temp_divisor << 1):
-        temp_divisor <<= 1
-        multiple <<= 1
+"""
+Bitwise LEFT SHIFT (<<)
+bits << (n) add zeroes to the right
+14  = 1110 << 1
+ans = 111110
+"""
+print("Bitwise LEFT SHIFT (<<)")
+a = 10
+ans = a << 5
+print(f'a = {bin(a)[2:]}')
+print(f'ans = {bin(ans)[2:]}')
+print("===============")
 
-    dividend -= temp_divisor
-    result += multiple
-
-x = 121
-ans = 0
-while x > 0:
-    r = x % 10
-    ans = (ans * 10) + r
-    x //= 10
-print(ans)
+"""
+Bitwise RIGHT SHIFT (>>)
+bits << (n) add zeroes to the left
+15  = 1010
+ans = 0101
+"""
+print("Bitwise RIGHT SHIFT (^)")
+a = 10
+ans = a >> 1
+print(f'a = {bin(a)[2:]}')
+print(f'ans = {bin(ans)[2:]}')
+print("===============")
