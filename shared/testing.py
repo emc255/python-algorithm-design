28480bd52dc296f56e0a42fc977579cdfa47e03a
michael <micha3l_255@yahoo.com>
2023-09-27 18:07:07 -0500

    test

--- shared/testing.py
+++ shared/testing.py
@@ -1,74 +1,37 @@
-# a = "ZY"
-# result = 0
-# for i in range(len(a)):
-#     print((ord(a[i]) - 64))
-#     print(25 ** len(a) - i)
-#     result += (ord(a[i]) - 64) + (25 ** len(a) - i)
-# print(result)
+from collections import deque
 
+tickets = [["JFK", "SFO"], ["JFK", "ATL"], ["SFO", "ATL"], ["ATL", "JFK"], ["ATL", "SFO"]]
 
-arr = [1, 2, 3, 4, 5, 6]
-left = 1
-right = 3
-
-for i in range(len(arr)):
-    if i == left:
-        while left < right:
-            arr[left], arr[right] = arr[right], arr[left]
-            left += 1
-            right -= 1
-
-print(arr)
-
-n = 110
-rn = 0
-while n > 0:
-    rn = rn | (n & 1)
-    rn <<= 1
-    n >>= 1
-
-print(rn)
-
-memo = {}
-s = "abcabc"
-for i in range(len(s)):
-    if s[i] in memo:
-        memo[s[i]] += 1
+map = {}
+temp_key = None
+test = []
+for ticket in tickets:
+    if temp_key is None:
+        temp_key = tuple(ticket)
     else:
-        memo[s[i]] = 1
+        departure, destination = temp_key
+        if ticket[0] < departure or ticket[0] == departure and ticket[1] < destination:
+            temp_key = ticket
 
-remove = 0
-memo = dict(sorted(memo.items(), key=lambda item: item[1], reverse=True))
+    map[ticket[0]] = ticket
 
-d_list = list(memo.values())
-print(d_list)
+map.pop(temp_key[0])
+test.append(temp_key)
+my_queue = deque()
+# while map:
+#
+#     original_departure = temp_key[0]
+#     original_destination = temp_key[1]
+#     while original_destination in map:
+#         my_queue.append(map[original_destination])
+#
+#     while my_queue:
+#         departure, destination = my_queue.pop()
+#         if original_departure < departure or original_departure == departure and original_destination < destination:
+#             print(my_queue)
 
-sort = True
-temp = 0
-while sort and temp < len(d_list):
-    sort = False
-    for i in range(len(d_list)):
-        sort = True
-        if i != temp and d_list[i] == d_list[temp]:
-            d_list[temp] -= 1
-            remove += 1
-            if d_list[temp] == 0:
-                d_list.pop(temp)
 
-    temp += 1
+print(test)
+print(temp_key)
 
-print(remove)
-# for k, v in memo.items():
-#     if temp:
-#         tk, tv = temp
-#         if tv == v:
-#             remove += 1
-#             tv -= 1
-#             if tv == 0:
-#                 temp = ()
-#             else:
-#                 temp = (tk, tv)
-#     else:
-#         temp = (k, v)
-#     print(f'{k} = {v}')
-print(remove)
+print(map)
