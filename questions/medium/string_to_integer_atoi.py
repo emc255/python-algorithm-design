63e3d6fe211778ab39ab9368bbb6c29f171ff85f
emc <micha3l_255@yahoo.com>
2023-10-09 03:32:28 -0500

    string to integer

---
+++ questions/medium/string_to_integer_atoi.py
@@ -0,0 +1,106 @@
+"""
+8. String to Integer (atoi)
+
+Implement the myAtoi(string s) function, which converts a string to a 32-bit signed integer (similar to C/C++'s atoi function).
+
+The algorithm for myAtoi(string s) is as follows:
+
+Read in and ignore any leading whitespace.
+Check if the next character (if not already at the end of the string) is '-' or '+'.
+Read this character in if it is either. This determines if the final result is negative or positive respectively.
+Assume the result is positive if neither is present.
+Read in next the characters until the next non-digit character or the end of the input is reached.
+The rest of the string is ignored.
+Convert these digits into an integer (i.e. "123" -> 123, "0032" -> 32).
+If no digits were read, then the integer is 0.
+Change the sign as necessary (from step 2).
+If the integer is out of the 32-bit signed integer range [-231, 231 - 1],
+then clamp the integer so that it remains in the range. Specifically,
+integers less than -231 should be clamped to -231,
+and integers greater than 231 - 1 should be clamped to 231 - 1.
+Return the integer as the final result.
+
+Note:
+Only the space character ' ' is considered a whitespace character.
+Do not ignore any characters other than the leading whitespace or the rest of the string after the digits.
+
+Example 1:
+Input: s = "42"
+Output: 42
+Explanation: The underlined characters are what is read in, the caret is the current reader position.
+Step 1: "42" (no characters read because there is no leading whitespace)
+         ^
+Step 2: "42" (no characters read because there is neither a '-' nor '+')
+         ^
+Step 3: "42" ("42" is read in)
+           ^
+The parsed integer is 42.
+Since 42 is in the range [-231, 231 - 1], the final result is 42.
+
+Example 2:
+Input: s = "   -42"
+Output: -42
+Explanation:
+Step 1: "   -42" (leading whitespace is read and ignored)
+            ^
+Step 2: "   -42" ('-' is read, so the result should be negative)
+             ^
+Step 3: "   -42" ("42" is read in)
+               ^
+The parsed integer is -42.
+Since -42 is in the range [-231, 231 - 1], the final result is -42.
+
+Example 3:
+Input: s = "4193 with words"
+Output: 4193
+Explanation:
+Step 1: "4193 with words" (no characters read because there is no leading whitespace)
+         ^
+Step 2: "4193 with words" (no characters read because there is neither a '-' nor '+')
+         ^
+Step 3: "4193 with words" ("4193" is read in; reading stops because the next character is a non-digit)
+             ^
+The parsed integer is 4193.
+Since 4193 is in the range [-231, 231 - 1], the final result is 4193.
+
+Constraints:
+0 <= s.length <= 200
+s consists of English letters (lower-case and upper-case), digits (0-9), ' ', '+', '-', and '.'.
+
+"""
+
+
+def my_atoi(s: str) -> int:
+    string_result = ""
+    index = 0
+    negative = 1
+
+    while index < len(s) and s[index] == " ":
+        index += 1
+
+    if index < len(s) and s[index] in "+-":
+        negative = -1 if s[index] == "-" else 1
+        index += 1
+
+    while index < len(s):
+        if s[index] in "1234567890":
+            string_result += s[index]
+            index += 1
+        else:
+            break
+
+    result = int(string_result) if len(string_result) > 0 else 0
+    result = negative * result
+    max_size = 2 ** 31 - 1
+    min_size = -2 ** 31
+
+    return min(max_size, result) if result > 0 else max(min_size, result)
+
+
+print(my_atoi("4193 with words"))
+print(my_atoi("-4193"))
+print(my_atoi("-+42"))
+print(my_atoi("21474836460"))
+print(my_atoi("0000-1"))
+print(my_atoi("  0000000000012345678"))
+print(my_atoi("   +0 123"))
