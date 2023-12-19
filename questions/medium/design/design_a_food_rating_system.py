15b89e6ece36cffaaddeca14408eb94891097794
emc <micha3l_255@yahoo.com>
2023-12-18 21:30:53 -0800

    Design a Food Rating System

--- questions/medium/design/design_a_food_rating_system.py
+++ questions/medium/design/design_a_food_rating_system.py
@@ -69,15 +69,55 @@ At most 2 * 104 calls in total will be made to changeRating and highestRated.
 
 
 """
+from collections import defaultdict
+
+from icecream import ic
+from sortedcontainers import SortedSet
 
 
 class FoodRatings:
 
     def __init__(self, foods: list[str], cuisines: list[str], ratings: list[int]):
-        pass
+        # Map food with its rating.
+        self.food_rating_map = {}
+        # Map food with cuisine it belongs to.
+        self.food_cuisine_map = {}
+
+        # Store all food of a cuisine in a set (to sort them on ratings/name)
+        # Set element -> Tuple: (-1 * food_rating, food_name)
+        self.cuisine_food_map = defaultdict(SortedSet)
+
+        for i in range(len(foods)):
+            # Store 'rating' and 'cuisine' of the current 'food' in 'food_rating_map' and 'food_cuisine_map' maps.
+            self.food_rating_map[foods[i]] = ratings[i]
+            self.food_cuisine_map[foods[i]] = cuisines[i]
+            # Insert the '(-1 * rating, name)' element in the current cuisine's set.
+            self.cuisine_food_map[cuisines[i]].add((-ratings[i], foods[i]))
+        ic(self.food_rating_map)
+        ic(self.food_cuisine_map)
 
     def change_rating(self, food: str, new_rating: int) -> None:
-        pass
+        # Fetch cuisine name for food.
+        cuisine_name = self.food_cuisine_map[food]
+
+        # Find and delete the element from the respective cuisine's set.
+        old_element = (-self.food_rating_map[food], food)
+        self.cuisine_food_map[cuisine_name].remove(old_element)
+
+        # Update food's rating in 'food_rating' map.
+        self.food_rating_map[food] = new_rating
+        # Insert the '(-1 * new rating, name)' element in the respective cuisine's set.
+        self.cuisine_food_map[cuisine_name].add((-new_rating, food))
 
     def highest_rated(self, cuisine: str) -> str:
-        pass
+        highest_rated = self.cuisine_food_map[cuisine][0]
+        # Return name of the highest-rated 'food' of 'cuisine'.
+        return highest_rated[1]
+
+
+foodie = FoodRatings(["kimchi", "miso", "sushi", "moussaka", "ramen", "bulgogi"],
+                     ["korean", "japanese", "japanese", "greek", "japanese", "korean"],
+                     [9, 12, 8, 15, 14, 7])
+foodie.change_rating("sushi", 22)
+foodie.change_rating("ramen", 22)
+ic(foodie.highest_rated("japanese"))
