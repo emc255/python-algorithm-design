1f3abf1d9f44a4fb3f36241bd31bbc935c66d998
emc <micha3l_255@yahoo.com>
2024-01-07 13:24:02 -0800

    Arithmetic Slices II - Subsequence

--- questions/hard/dynamic_programming/arithmetic_slices_subsequence_II.py
+++ questions/hard/dynamic_programming/arithmetic_slices_subsequence_II.py
@@ -38,11 +38,40 @@ Constraints:
 -231 <= nums[i] <= 231 - 1
 
 """
+from collections import defaultdict
+
 from icecream import ic
 
 
 def number_of_arithmetic_slices(nums: list[int]) -> int:
-    return 0
+    result = 0
+    n = len(nums)
+    dp = [defaultdict(int) for _ in range(n)]
+    #  dp[i][difference] -> # of subsequence ending at i, with difference
+    for i in range(n):
+        for j in range(i):
+            difference = nums[i] - nums[j]
+            dp[i][difference] += 1 + dp[j][difference]
+            result += 1 + dp[j][difference]
+    return result - (n * (n - 1) // 2)
+
+
+def number_of_arithmetic_slices_v2(nums: list[int]) -> int:
+    n = len(nums)
+    count = 0
+    subsequences = {}
+
+    for i in range(n):
+        subsequences[i] = {}
+        for j in range(i):
+            diff = nums[i] - nums[j]
+            if diff in subsequences[j]:
+                subsequences[i][diff] = subsequences[i].get(diff, 0) + subsequences[j][diff] + 1
+                count += subsequences[j][diff]
+            else:
+                subsequences[i][diff] = subsequences[i].get(diff, 0) + 1
+
+    return count
 
 
 ic(number_of_arithmetic_slices([2, 4, 6, 8, 10]))
