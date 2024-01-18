f5c11c672b794fe28110d235bb895eb0f13cb3ee
emc <micha3l_255@yahoo.com>
2024-01-17 22:00:21 -0800

    Jump Game

--- shared/file_namer.py
+++ shared/file_namer.py
@@ -17,8 +17,8 @@ def camel_to_snake_case(s: str) -> str:
     return ''.join(new_name)
 
 
-filename = "Unique Number of Occurrences"
+filename = "Jump Game"
 ic(format_file_name(filename))
 
-func = "uniqueOccurrences"
+func = "canJump"
 ic(camel_to_snake_case(func))
