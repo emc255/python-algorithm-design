714641e924431e1bbad75f152f0d7c37a326e83b
emca <micha3l_255@yahoo.com>
2024-03-15 02:29:09 -0700

    Product of Array Except Self

--- shared/file_namer.py
+++ shared/file_namer.py
@@ -17,8 +17,8 @@ def camel_to_snake_case(s: str) -> str:
     return ''.join(new_name)
 
 
-filename = "Find the Pivot Integer"
+filename = "Product of Array Except Self"
 ic(format_file_name(filename))
 
-func = "pivotInteger"
+func = "productExceptSelf"
 ic(camel_to_snake_case(func))
