152e53a742392f32e495c3445a2e9b112752a489
emca <micha3l_255@yahoo.com>
2024-04-10 12:08:22 -0700

    Reveal Cards In Increasing Order

--- shared/file_namer.py
+++ shared/file_namer.py
@@ -17,8 +17,8 @@ def camel_to_snake_case(s: str) -> str:
     return ''.join(new_name)
 
 
-filename = "Number of Students Unable to Eat Lunch"
+filename = "Reveal Cards In Increasing Order"
 ic(format_file_name(filename))
 
-func = "countStudents"
+func = "deckRevealedIncreasing"
 ic(camel_to_snake_case(func))
