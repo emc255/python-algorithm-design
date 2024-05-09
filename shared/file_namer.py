815f35a3c2c8af517e513044e1bccc92876a0803
emca <micha3l_255@yahoo.com>
2024-05-09 11:04:40 -0700

    Maximize Happiness of Selected Children

--- shared/file_namer.py
+++ shared/file_namer.py
@@ -17,8 +17,8 @@ def camel_to_snake_case(s: str) -> str:
     return ''.join(new_name)
 
 
-filename = "Double a Number Represented as a Linked List"
+filename = "Maximize Happiness of Selected Children"
 ic(format_file_name(filename))
 
-func = "doubleIt"
+func = "maximumHappinessSum"
 ic(camel_to_snake_case(func))
