e6ef60b686597b6898df54539e801ce518ace02a
emc <micha3l_255@yahoo.com>
2024-05-11 03:09:43 -0700

    Minimum Cost to Hire K Workers

--- shared/file_namer.py
+++ shared/file_namer.py
@@ -17,8 +17,8 @@ def camel_to_snake_case(s: str) -> str:
     return ''.join(new_name)
 
 
-filename = "K-th Smallest Prime Fraction"
+filename = "Minimum Cost to Hire K Workers"
 ic(format_file_name(filename))
 
-func = "kthSmallestPrimeFraction"
+func = "minimumcostToHireWorkers"
 ic(camel_to_snake_case(func))
