53bf454d24776a8d3149599b4e62504636b02d16
emca <micha3l_255@yahoo.com>
2024-05-31 22:02:00 -0700

    Score of a String

--- shared/file_namer.py
+++ shared/file_namer.py
@@ -17,8 +17,8 @@ def camel_to_snake_case(s: str) -> str:
     return ''.join(new_name)
 
 
-filename = "Single Number III"
+filename = "Score of a String"
 ic(format_file_name(filename))
 
-func = "singleNumber"
+func = "scoreOfString"
 ic(camel_to_snake_case(func))
