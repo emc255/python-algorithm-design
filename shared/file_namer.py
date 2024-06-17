73e9793b76b2de003d0acf7fb0a0a8d2ad02547d
emc <micha3l_255@yahoo.com>
2024-06-16 23:26:59 -0700

    IPO

--- shared/file_namer.py
+++ shared/file_namer.py
@@ -17,8 +17,8 @@ def camel_to_snake_case(s: str) -> str:
     return ''.join(new_name)
 
 
-filename = "Height Checker"
+filename = "IPO"
 ic(format_file_name(filename))
 
-func = "heightChecker"
+func = "findMaximizedCapital"
 ic(camel_to_snake_case(func))
