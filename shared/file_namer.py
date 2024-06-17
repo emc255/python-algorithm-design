927d0108d1d5d2b8c7b54bfbe39899ce778b5874
emc <micha3l_255@yahoo.com>
2024-06-16 23:30:31 -0700

    Patching Array

--- shared/file_namer.py
+++ shared/file_namer.py
@@ -17,8 +17,8 @@ def camel_to_snake_case(s: str) -> str:
     return ''.join(new_name)
 
 
-filename = "IPO"
+filename = "Patching Array"
 ic(format_file_name(filename))
 
-func = "findMaximizedCapital"
+func = "minPatches"
 ic(camel_to_snake_case(func))
