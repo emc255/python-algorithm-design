8c1e245051692ccbdd9a432c00f7f1e07cd5b050
emca <micha3l_255@yahoo.com>
2024-07-15 11:37:38 -0700

    Create Binary Tree From Descriptions

--- shared/file_namer.py
+++ shared/file_namer.py
@@ -17,8 +17,8 @@ def camel_to_snake_case(s: str) -> str:
     return ''.join(new_name)
 
 
-filename = "Number of Atoms"
+filename = "Create Binary Tree From Descriptions"
 ic(format_file_name(filename))
 
-func = "countOfAtoms"
+func = "createBinaryTree"
 ic(camel_to_snake_case(func))
