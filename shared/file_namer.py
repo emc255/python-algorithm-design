b1a93dd6b28ecd6effcc45e14bbb080dbb87a944
emca <micha3l_255@yahoo.com>
2024-07-19 23:57:55 -0700

    Find Valid Matrix Given Row and Column Sums

--- shared/file_namer.py
+++ shared/file_namer.py
@@ -17,8 +17,8 @@ def camel_to_snake_case(s: str) -> str:
     return ''.join(new_name)
 
 
-filename = "Number of Good Leaf Nodes Pairs"
+filename = "Find Valid Matrix Given Row and Column Sums"
 ic(format_file_name(filename))
 
-func = "countPairs"
+func = "restoreMatrix"
 ic(camel_to_snake_case(func))
