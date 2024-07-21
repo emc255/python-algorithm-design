d367ef29e386c044cefcfdcaf7ec35eeb021d207
emc <micha3l_255@yahoo.com>
2024-07-20 23:34:35 -0700

    Build a Matrix With Conditions

--- shared/file_namer.py
+++ shared/file_namer.py
@@ -17,8 +17,8 @@ def camel_to_snake_case(s: str) -> str:
     return ''.join(new_name)
 
 
-filename = "Find Valid Matrix Given Row and Column Sums"
+filename = "Build a Matrix With Conditions"
 ic(format_file_name(filename))
 
-func = "restoreMatrix"
+func = "buildMatrix"
 ic(camel_to_snake_case(func))
