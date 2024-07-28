576cb8c817ccacccb9a6402d2fa39e21e405434b
emca <micha3l_255@yahoo.com>
2024-07-28 13:48:38 -0700

    Second Minimum Time to Reach Destination

--- shared/file_namer.py
+++ shared/file_namer.py
@@ -17,8 +17,8 @@ def camel_to_snake_case(s: str) -> str:
     return ''.join(new_name)
 
 
-filename = "Minimum Cost to Convert String I"
+filename = "Second Minimum Time to Reach Destination"
 ic(format_file_name(filename))
 
-func = "minimumCost"
+func = "secondMinimum"
 ic(camel_to_snake_case(func))
