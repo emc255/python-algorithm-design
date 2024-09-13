a3229abad987c09dea69119ed67bf6258d64bfb2
emc <micha3l_255@yahoo.com>
2024-09-13 12:23:21 -0700

    Count the Number of Consistent Strings

--- shared/file_namer.py
+++ shared/file_namer.py
@@ -17,8 +17,8 @@ def camel_to_snake_case(s: str) -> str:
     return ''.join(new_name)
 
 
-filename = "Minimum Bit Flips to Convert Number"
+filename = "Count the Number of Consistent Strings"
 ic(format_file_name(filename))
 
-func = "minimumBitFlips"
+func = "countConsistentStrings"
 ic(camel_to_snake_case(func))
