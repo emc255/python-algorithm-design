83adc11ac8f9f25cd918a6bc4ed33fda5127467b
emca <micha3l_255@yahoo.com>
2024-09-21 15:00:46 -0700

    Lexicographical Numbers

---
+++ questions/medium/integer/lexicographical_numbers.py
@@ -0,0 +1,57 @@
+"""
+386. Lexicographical Numbers
+
+Given an integer n,
+return all the numbers in the range [1, n]
+sorted in lexicographical order.
+
+You must write an algorithm that runs
+in O(n) time and uses O(1) extra space.
+
+Example 1:
+Input: n = 13
+Output: [1,10,11,12,13,2,3,4,5,6,7,8,9]
+
+Example 2:
+Input: n = 2
+Output: [1,2]
+
+Constraints:
+1 <= n <= 5 * 104
+
+"""
+from icecream import ic
+
+
+def lexical_order(n: int) -> list[int]:
+    result = []
+    current = 1
+    for _ in range(n):
+        result.append(current)
+        if current * 10 <= n:
+            current *= 10
+        else:
+            if current >= n:
+                current //= 10
+            current += 1
+            while current % 10 == 0:
+                current //= 10
+    return result
+
+
+def lexical_order_v2(n: int) -> list[int]:
+    res = []
+    num = 1
+    for _ in range(n):
+        res.append(num)
+        if num * 10 <= n:
+            num *= 10
+        else:
+            while num % 10 == 9 or num + 1 > n:
+                num //= 10
+            num += 1
+    return res
+
+
+ic(lexical_order(33))
+ic(lexical_order_v2(222))
--- shared/file_namer.py
+++ shared/file_namer.py
@@ -17,8 +17,8 @@ def camel_to_snake_case(s: str) -> str:
     return ''.join(new_name)
 
 
-filename = "Largest Number"
+filename = "Lexicographical Numbers"
 ic(format_file_name(filename))
 
-func = "largestNumber"
+func = "lexicalOrder"
 ic(camel_to_snake_case(func))
