454bf7ae4adfa91d0edd52e61e050e9c471248d7
emca <micha3l_255@yahoo.com>
2024-09-24 00:26:21 -0700

    Find the Length of the Longest Common Prefix

--- shared/file_namer.py
+++ shared/file_namer.py
@@ -17,8 +17,8 @@ def camel_to_snake_case(s: str) -> str:
     return ''.join(new_name)
 
 
-filename = "Extra Characters in a String"
+filename = "Find the Length of the Longest Common Prefix"
 ic(format_file_name(filename))
 
-func = "minimumExtraChar"
+func = "longestCommonPrefix"
 ic(camel_to_snake_case(func))
