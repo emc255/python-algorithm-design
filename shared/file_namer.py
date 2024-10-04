946d99adb27f702c7c1d6c2d16d0d057ab949d79
emca <micha3l_255@yahoo.com>
2024-10-04 13:44:58 -0700

    Divide Players Into Teams of Equal Skill

--- shared/file_namer.py
+++ shared/file_namer.py
@@ -17,8 +17,8 @@ def camel_to_snake_case(s: str) -> str:
     return ''.join(new_name)
 
 
-filename = "Design a Stack With Increment Operation"
+filename = "Divide Players Into Teams of Equal Skill"
 ic(format_file_name(filename))
 
-func = "sumPrefixScores"
+func = "dividePlayers"
 ic(camel_to_snake_case(func))
