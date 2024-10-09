ada09699eaea8038f7286f5ad36394a988f4c9f3
emca <micha3l_255@yahoo.com>
2024-10-08 22:23:08 -0700

    Minimum Add to Make Parentheses Valid

--- shared/file_namer.py
+++ shared/file_namer.py
@@ -17,8 +17,8 @@ def camel_to_snake_case(s: str) -> str:
     return ''.join(new_name)
 
 
-filename = "Minimum Number of Swaps to Make the String Balanced"
+filename = "Minimum Add to Make Parentheses Valid"
 ic(format_file_name(filename))
 
-func = "minimumSwaps"
+func = "minimumAddToMakeValid"
 ic(camel_to_snake_case(func))
