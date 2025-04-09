5102139346907b11560500105c82e083fcee948c
emc <micha3l_255@yahoo.com>
2025-04-09 11:18:15 -0700

    Minimum Operations to Make Array Values Equal to K

--- shared/file_namer.py
+++ shared/file_namer.py
@@ -17,7 +17,7 @@ def camel_to_snake_case(s: str) -> str:
     return ''.join(new_name)
 
 
-filename = "Minimum Number of Operations to Make Elements in Array Distinct"
+filename = "Minimum Operations to Make Array Values Equal to K"
 ic(format_file_name(filename))
 
 func = "minimumOperations"
