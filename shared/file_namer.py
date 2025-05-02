584c14a84934a07cab14a573d864af80ed9f6731
emca <micha3l_255@yahoo.com>
2025-05-02 12:38:26 -0700

    Push Dominoes

--- shared/file_namer.py
+++ shared/file_namer.py
@@ -17,8 +17,8 @@ def camel_to_snake_case(s: str) -> str:
     return ''.join(new_name)
 
 
-filename = ""
+filename = "Push Dominoes"
 ic(format_file_name(filename))
 
-func = ""
+func = "pushDominoes"
 ic(camel_to_snake_case(func))
