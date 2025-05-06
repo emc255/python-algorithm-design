d68f930f8c86716707c9515b946baa4c9528271e
emca <micha3l_255@yahoo.com>
2025-05-05 23:07:27 -0700

    Build Array from Permutation

--- shared/file_namer.py
+++ shared/file_namer.py
@@ -17,8 +17,8 @@ def camel_to_snake_case(s: str) -> str:
     return ''.join(new_name)
 
 
-filename = "Minimum Domino Rotations For Equal Row"
+filename = "Build Array from Permutation"
 ic(format_file_name(filename))
 
-func = "minDominoRotations"
+func = "buildArray"
 ic(camel_to_snake_case(func))
