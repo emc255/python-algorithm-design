acff4264f51d99a4df60d122f0f16fc11ade3ab7
emca <micha3l_255@yahoo.com>
2025-05-12 21:27:43 -0700

    Total Characters in String After Transformations I

--- shared/file_namer.py
+++ shared/file_namer.py
@@ -17,8 +17,8 @@ def camel_to_snake_case(s: str) -> str:
     return ''.join(new_name)
 
 
-filename = "Find Minimum Time to Reach Last Room I"
+filename = "Total Characters in String After Transformations I"
 ic(format_file_name(filename))
 
-func = "minimumTimeToReach"
+func = "lengthAfterTransformations "
 ic(camel_to_snake_case(func))
