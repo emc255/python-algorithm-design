1010200297c1dbb13bfe6643c26a1dc681865612
emca <micha3l_255@yahoo.com>
2025-06-04 22:23:14 -0700

    Lexicographically Smallest Equivalent String

--- shared/file_namer.py
+++ shared/file_namer.py
@@ -17,8 +17,8 @@ def camel_to_snake_case(s: str) -> str:
     return ''.join(new_name)
 
 
-filename = "Find the Lexicographically Largest String From the Box I"
+filename = "Lexicographically Smallest Equivalent String"
 ic(format_file_name(filename))
 
-func = "answerString"
+func = "smallestEquivalentString"
 ic(camel_to_snake_case(func))
