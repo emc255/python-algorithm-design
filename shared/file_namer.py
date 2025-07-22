85a3510e409659fa6abb8880b121f706bdd2045a
emca <micha3l_255@yahoo.com>
2025-07-21 17:45:36 -0700

    Maximum Erasure Value

--- shared/file_namer.py
+++ shared/file_namer.py
@@ -17,8 +17,8 @@ def camel_to_snake_case(s: str) -> str:
     return ''.join(new_name)
 
 
-filename = "Unique Binary Search Trees"
+filename = "Maximum Erasure Value"
 ic(format_file_name(filename))
 
-func = "numTrees"
+func = "maximumUniqueSubarray"
 ic(camel_to_snake_case(func))
